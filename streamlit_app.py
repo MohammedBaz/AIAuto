124987bedb01a6fd7faa4cb8c5259a626baa65d1
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2024-12-17 18:22:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit as st
 
-st.write("Hello, *World!* :sunglasses:")
-
-messages = st.container(height=300)
-if prompt := st.chat_input("Say something"):
-    messages.chat_message("user").write(prompt)
-    messages.chat_message("assistant").write(f"Echo: {prompt}")
+with st.chat_message("user"):
+    st.write("Hello 👋")
